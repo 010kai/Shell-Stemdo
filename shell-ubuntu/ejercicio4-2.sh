9134e6836219d4efbb5ad58951a37202022013e5
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:08:10 +0100

    ejercicio4-2.sh

--- shell-ubuntu/ejercicio4-2.sh
+++ shell-ubuntu/ejercicio4-2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-read -p "Introduce un nombre de usuario: " usu
+read -p "Introduce un nombre de usuario: " usu  # Pide un nombre de usuario
 
-ps -u $usu | sort -k 4
+ps -u "$usu" | sort -k 4  # Muestra los procesos del usuario y los ordena por la cuarta columna (tiempo de CPU)
