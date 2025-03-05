f51b47b355068b4eb969a21a7b81c84426ee8369
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:06:41 +0100

    ejercicio3-2.sh

--- shell-ubuntu/ejercicio3-2.sh
+++ shell-ubuntu/ejercicio3-2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read -p "Escribe tu peso en Kg: " kg
+read -p "Escribe tu peso en Kg: " kg  # Pide el peso en kg
 
-read -p "Escribe tu altura en metros: " m
+read -p "Escribe tu altura en metros: " m  # Pide la altura en metros
 
-imc=$(echo "scale=2; $kg / ($m * $m)" | bc)
+imc=$(echo "scale=2; $kg / ($m * $m)" | bc)  # Calcula el IMC usando bc para mayor precisión
 
-echo "Tu IMC es de $imc"
+echo "Tu IMC es de $imc"  # Muestra el IMC calculado
