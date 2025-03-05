d96b2e312baf7d86b452f7df15510f673d965928
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:15:36 +0100

    ejercicio3-ssoo.sh

--- administracion ssoo/ejercicio3-ssoo.sh
+++ administracion ssoo/ejercicio3-ssoo.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-read -p "Introduce el nombre del fichero que creaste en el ejercicio 2: " fic
-al="$HOME/$fic"
+read -p "Introduce el nombre del fichero que creaste en el ejercicio 2: " fic  # Solicita el nombre del fichero
+al="$HOME/$fic"  # Asigna la ruta completa del fichero
 
-if [ -e "$al" ]; then
-    if [ -f "$al" ]; then
-        if [ -r "$al" ]; then
+if [ -e "$al" ]; then  # Verifica si el fichero existe
+    if [ -f "$al" ]; then  # Verifica si es un fichero regular
+        if [ -r "$al" ]; then  # Verifica si tiene permisos de lectura
             echo "El fichero '$fic' tiene permisos de lectura. Mostrando contenido..."
 
-            # Mostrar el contenido en formato tabla usando cat, awk y printf
+            # Muestra el contenido en formato tabla usando awk
             echo "--------------------------------------------"
             awk -F ':' 'BEGIN { printf "| %-3s | %-10s | %-10s | %-10s |\n", "Nº", "Nombre", "Apellido1", "Apellido2"; print "|-----|------------|------------|------------|"} 
             { printf "| %-3s | %-10s | %-10s | %-10s |\n", $1, $2, $3, $4 }' "$al"
             echo "--------------------------------------------"
 
         else
-            echo "No tienes permisos de lectura sobre el fichero '$fic'."
+            echo "No tienes permisos de lectura sobre el fichero '$fic'."  # Si no tiene permisos de lectura
         fi
     else
-        echo "'$fic' no es un fichero regular."
+        echo "'$fic' no es un fichero regular."  # Si no es un fichero regular
     fi
 else
-    echo "El fichero '$fic' no existe en tu directorio de inicio."
+    echo "El fichero '$fic' no existe en tu directorio de inicio."  # Si el fichero no existe
 fi
