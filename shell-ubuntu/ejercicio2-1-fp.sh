7abdaf4a0f442036b7056cb98d7d44dd806f5b69
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:04:43 +0100

    ejercicio2-1-fp.sh

--- shell-ubuntu/ejercicio2-1-fp.sh
+++ shell-ubuntu/ejercicio2-1-fp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-read -p "Introduce 3 valores: " n1 n2 n3
+read -p "Introduce 3 valores: " n1 n2 n3  # Pide 3 valores
 
-n1=${n1:-NULL}
+n1=${n1:-NULL}  # Si está vacío, usa "NULL"
 n2=${n2:-NULL}
 n3=${n3:-NULL}
 
-echo "$n1 $n2 $n3"
+echo "$n1 $n2 $n3"  # Muestra los valores
 
-all=$n1$n2$n3
+all=$n1$n2$n3  # Concatena los valores
 
-echo "$all"
+echo "$all"  # Muestra la concatenación
