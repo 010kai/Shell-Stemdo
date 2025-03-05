74778da89b53410da82b23013261eb571f55cd46
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:06:19 +0100

    ejercicio3-1.sh

--- shell-ubuntu/ejercicio3-1.sh
+++ shell-ubuntu/ejercicio3-1.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
 
-read -p "Ingrese dos numeros: " num1 num2
+read -p "Ingrese dos números: " num1 num2  # Pide dos números
 
-read -p "¿Que quieres hacer con estos 2 numeros (+,x,/,-)?: " val
+read -p "¿Qué quieres hacer con estos 2 números (+,x,/,-)?: " val  # Pide la operación
 
-if [ $val == '+' ]
-then
+if [ $val == '+' ]; then
+  sum=$(( $num1 + $num2 ))  # Suma
+  echo "El resultado de $num1 + $num2 es $sum"
 
-sum=$(( $num1 + $num2 ))
-
-echo "El resultado de $num1 + $num2 es $sum"
-
-elif [ $val == '-' ]
-then
-res=$(( $num1 - $num2 ))
-
-echo "El resultado de $num1 - $num2 es $res"
+elif [ $val == '-' ]; then
+  res=$(( $num1 - $num2 ))  # Resta
+  echo "El resultado de $num1 - $num2 es $res"
 fi
 
-if [ $val == 'x' ]
-then
+if [ $val == 'x' ]; then
+  mul=$(( $num1 * $num2 ))  # Multiplicación
+  echo "El resultado de $num1 x $num2 es $mul"
 
-mul=$(( $num1 * $num2 ))
-
-echo "El resultado de $num1 x $num2 es $mul "
-
-elif [ $val == '/' ]
-then
-
-dic=$(( $num1 / $num2 ))
-echo "El resultado de $num1 / $num2 es $dic"
+elif [ $val == '/' ]; then
+  dic=$(( $num1 / $num2 ))  # División
+  echo "El resultado de $num1 / $num2 es $dic"
 fi
