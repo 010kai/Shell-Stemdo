ea1e25562b6ba8e53a66e8082d14fb9d3590440a
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:05:11 +0100

    ejercicio2-2-fp.sh

--- shell-ubuntu/ejercicio2-2-fp.sh
+++ shell-ubuntu/ejercicio2-2-fp.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-echo "Mostrando valores en colmna"
+echo "Mostrando valores en columna"
 
-printf "%-10s %-10s %-10s\n" "Ciudad" "Pueblo" "Pais"
+printf "%-10s %-10s %-10s\n" "Ciudad" "Pueblo" "Pais"  # Encabezado
 printf "%-10s %-10s %-10s\n" "Huelva" "Lepe" "Espana"
 printf "%-10s %-10s %-10s\n" "Sevilla" "Chucena" "Espana"
 
 echo ""
 
-
-echo "Mostrando numeros con decimales"
+echo "Mostrando números con decimales"
 valor=33,123
-printf "Valor: %.2f\n" $valor
+printf "Valor: %.2f\n" $valor  # Error: usa ',' en vez de '.'
 
 peso=55,241
-printf "Peso: %.1f\n" $peso
+printf "Peso: %.1f\n" $peso  # Mismo problema que arriba
 echo ""
 
 echo "Caracteres especiales"
 
-printf "Hola buenos dias\tcon adios buenas noches\n"
-printf "\u2764\n"
+printf "Hola buenos días\tcon adiós buenas noches\n"  # Usa tabulación
+printf "\u2764\n"  # Imprime un corazón (❤️)
