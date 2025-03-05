0000a41882fecd06653b0a33ac5237c55157a0d4
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:10:34 +0100

    ejercicio5-2.sh

--- shell-ubuntu/ejercicio5-2.sh
+++ shell-ubuntu/ejercicio5-2.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-read -p "Introduce nombres de usuario: " usu
+read -p "Introduce nombres de usuario: " usu  # Pide los nombres de usuario
 
-for i in $usu
+for i in $usu  # Itera sobre cada nombre de usuario ingresado
 do
-gre=$(grep ^$i /etc/passwd | cut -f 1 -d':' | wc -l)
+  gre=$(grep ^$i /etc/passwd | cut -f 1 -d':' | wc -l)  # Verifica si el usuario existe en el sistema
 
-if [ $gre -eq 0 ]
-then
-
-echo "El usuario $i no existe"
-
-else
-echo "El usuario $i existe"
-fi
+  if [ $gre -eq 0 ]; then  # Si el usuario no existe
+    echo "El usuario $i no existe"
+  else  # Si el usuario existe
+    echo "El usuario $i existe"
+  fi
 done
