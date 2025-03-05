bff2537eaf7c8b0d88803f72634fa5b6d8ecdf17
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-05 23:05:38 +0100

    ejercicio2-3.sh

--- shell-ubuntu/ejercicio2-3.sh
+++ shell-ubuntu/ejercicio2-3.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
+tot=$(( ($(date -d "12/31" +%s) - $(date +%s)) / 86400 ))  # Calcula los días hasta el 31 de diciembre
 
-
-tot=$(( ($(date -d "12/31" +%s) - $(date +%s) ) / 86400 ))
-
-
-
-echo "Estos son los dias que quedan hasta el 31 de Diciembre" $tot
+echo "Estos son los días que quedan hasta el 31 de diciembre: $tot"  # Muestra el resultado
